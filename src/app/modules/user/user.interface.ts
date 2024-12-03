c857bc788826a0077b7ec047df88760c8c23ae8f
sowmik-sec <sowmik.sec@gmail.com>
2024-12-03 15:56:32 +0600

    create user interface and model

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,21 @@
+import { Model } from 'mongoose';
+
+export interface IUser {
+  name: {
+    firstName: string;
+    lastName: string;
+  };
+  email: string;
+  password: string;
+}
+
+// Put all user instance methods in this interface:
+export interface IUserMethods {
+  isPasswordMatched(
+    givenPassword: string,
+    savedPassword: string,
+  ): Promise<boolean>;
+}
+
+// Create a new Model type that knows about IUserMethods...
+export type UserModel = Model<IUser, object, IUserMethods>;
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,39 @@
+import { Schema } from 'mongoose';
+import { IUser, IUserMethods, UserModel } from './user.interface';
+import bcrypt from 'bcrypt';
+import config from '../../../config';
+const userSchema = new Schema<IUser, UserModel, IUserMethods>({
+  name: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  this.password = await bcrypt.hash(
+    this.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+  next();
+});
+
+userSchema.statics.isPasswordMatched = async function (
+  givenPassword: string,
+  savedPassword: string,
+): Promise<boolean> {
+  return await bcrypt.compare(givenPassword, savedPassword);
+};
