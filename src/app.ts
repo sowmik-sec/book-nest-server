c5d0f3ba0aabe22a08efc742006395410d1bbedf
sowmik-sec <sowmik.sec@gmail.com>
2024-12-03 17:01:02 +0600

    create routes for the app

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import express, { Application } from 'express';
 const app: Application = express();
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import routers from './app/routes';
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1', routers);
 
 export default app;
