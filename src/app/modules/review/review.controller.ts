a8f4949ad4b4d28489b231c972dec34cfd4c803b
sowmik-sec <sowmik.sec@gmail.com>
2024-12-05 11:25:13 +0600

    create review controller

---
+++ src/app/modules/review/review.controller.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from 'express';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { StatusCodes } from 'http-status-codes';
+import pick from '../../../shared/pick';
+import { paginationFields } from '../../../constants/pagination';
+import { IBookReview } from './review.interface';
+import { ReviewService } from './review.service';
+
+const createReview = catchAsync(async (req: Request, res: Response) => {
+  const { ...reviewData } = req.body;
+  const result = await ReviewService.createReview(reviewData);
+  sendResponse<IBookReview>(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Review created successfully',
+    data: result,
+  });
+});
+const getAllReviews = catchAsync(async (req: Request, res: Response) => {
+  const paginationOptions = pick(req.query, paginationFields);
+  const result = await ReviewService.getAllReviews(paginationOptions);
+  sendResponse<IBookReview[]>(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Review retrieved successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const updateReview = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { ...reviewData } = req.body;
+  const result = await ReviewService.updateReview(id, reviewData);
+  sendResponse<IBookReview>(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Review updated successfully',
+    data: result,
+  });
+});
+
+const deleteReview = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await ReviewService.deleteReview(id);
+  sendResponse<IBookReview>(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Review deleted successfully',
+    data: result,
+  });
+});
+
+export const ReviewController = {
+  createReview,
+  getAllReviews,
+  updateReview,
+  deleteReview,
+};
